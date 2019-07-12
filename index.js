c26de7abcb4a881ec47c997cebed0f364923cc99
Samantha Hyatt <shhyatt@gmail.com>
2019-07-12 22:13:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,5 +55,6 @@ function insertNodeAt(index, newNodeAddress, linkedList, collection){
 
 function deleteNodeAt(index, linkedList, collection){
   let previousNode;
-  let currentNode = head
+  let currentNode = headNode(linkedList, collection);
+  
 }
